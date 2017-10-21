fd7e305ec27fdbce03121f5beb14ba1ed088c180
Echoblast53 <arc275555@gmail.com>
2017-10-21 00:15:23 -0700

    Fix bugs 1.1

--- Userscripts/echo_vault.user.js
+++ Userscripts/echo_vault.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Torn Property Vault Tracker
-// @version      1.0
+// @version      1.1
 // @description  Infinitly Increase Memory of Vault
 // @author       Echoblast53
 // @match        https://www.torn.com/properties.php
@@ -10,12 +10,21 @@
 
 (()=>{
 	"use strict";
+	const curr_ver = 1.1;
+	//Reseting with new version
+	if (GM_getValue("version","1.0") < curr_ver) {
+		GM_setValue("version",curr_ver);
+		GM_setValue("history","{}");
+	}
 	$(document).ready(()=>{
 		$(document).ajaxComplete((event,xhr,settings) => {
-			if (settings.url.search("properties.php") != -1) {
+			if (settings.url.search("properties.php") != -1 && window.location.href.search("tab=vault") != -1) {
 				try {
-					console.log(JSON.parse(xhr.responseText));
+					JSON.parse(xhr.responseText);
 				} catch (err) {
+					$(".vault-trans-wrap .title-black").append($('<button title="Remove Corrupted Data">Purge</button>').click(()=>{
+						purge();
+					}))
 					onLoad();
 					clear();
 					display();
@@ -23,21 +32,13 @@
 			}
 		});
 	});
-	function onLoad(cb) {
+	function onLoad() {
 		var history = JSON.parse(GM_getValue("history","{}"));
 		$(".transaction").each((i,e)=>{
 			if (!$(e).parent().hasClass("title")) {
 				var day = $(e).find(".date .transaction-date").text().match(/(\d{4})\/(\d{2})\/(\d{2})/);
 				var time = $(e).find(".date .transaction-time").text().match(/(\d{2}):(\d{2}):(\d{2}) (AM|PM)/);
-				var date = Date.UTC(day[1],day[2]-1,day[3],(()=>{
-					if (time[4] === "PM")
-						time[1]+=12;
-					if (time[1] === 24)
-						return 12;
-					if (time[1] === 12)
-						return 0;
-					return time[1];
-				})(),time[2],time[3])/1000;
+				var date = Date.UTC(day[1],day[2]-1,day[3],time[1],time[2],time[3]);
 				if (typeof history[date] === 'undefined')
 					history[date] = {
 						username: $.trim($($(e).find(".user a")[0]).text()),
@@ -53,42 +54,37 @@
 					};
 			}
 		});
+		console.log(history);
 		GM_setValue("history",JSON.stringify(history));
 	}
 	function display() {
 		var history = JSON.parse(GM_getValue("history","{}"));
-		var temp_history = {};
-		Object.keys(history).sort((a, b) => {return parseInt(b)-parseInt(a);}).forEach((key)=>{
-			$(".vault-trans-list").append(htmlTrans(key,history[key]));
+		Object.keys(history).sort((a, b) => {return parseInt(b)-parseInt(a);}).forEach((key,i)=>{
+			$(".vault-trans-list").append(htmlTrans(key,history[key],i));
 		});
 	}
-	function clear(cb) {
+	function clear() {
 		$(".transaction").each((i,e)=>{
 			if (!$(e).parent().hasClass("title")) {
 				$(e).parent().remove();
 			}
 		});
 	}
-	function htmlTrans(key,obj){
-		var date = new Date(key*1000);
-		return `<li><ul class="transaction"><li class="date"><span class="transaction-date">${pad(date.getUTCFullYear(),2)}/${pad(date.getUTCMonth()+1,2)}/${pad(date.getUTCDate(),2)}</span>
-		<span class="transaction-time">${pad((()=>{
-			var calc = date.getUTCHours()%12;
-			if (calc === 0)
-				return 12;
-			return calc;
-		})(),2)}:${pad(date.getUTCMinutes(),2)}:${pad(date.getUTCSeconds(),2)} ${(()=>{
+	function htmlTrans(key,obj,i){
+		var date = new Date(parseInt(key));
+		return `<li><ul class="transaction" ${(()=>{if(i>9)return 'style="opacity: 0.9;"';})()}><li class="date"><span class="transaction-date">${date.getUTCFullYear()}/${pad(date.getUTCMonth()+1,2)}/${pad(date.getUTCDate(),2)}</span><span class="transaction-time">${pad(date.getUTCHours(),2)}:${pad(date.getUTCMinutes(),2)}:${pad(date.getUTCSeconds(),2)} ${(()=>{
 			if (date.getUTCHours()/12 >= 1)
 				return "PM";
 			return "AM";
-		})()}</span></li>
-		<li class="user t-overflow"><span class="t-hide"><a class="user name" href="${obj.userlink}">${obj.username}</a></span>
-		<span class="d-hide"><a class="user name" href="${obj.userlink}">${obj.username}</a></span></li>
-		<li class="type">${obj.type}</li><li class="amount t-overflow"><span class="type-sign">${obj.type_sign}</span> ${obj.amount}</li><li class="balance t-overflow">${obj.balance}</li><li class="clear"></li></ul></li>`;
+		})()}</span></li><li class="user t-overflow"><span class="t-hide"><a class="user name" href="${obj.userlink}">${obj.username}</a></span><span class="d-hide"><a class="user name" href="${obj.userlink}">${obj.username}</a></span></li><li class="type">${obj.type}</li><li class="amount t-overflow"><span class="type-sign">${obj.type_sign}</span> ${obj.amount}</li><li class="balance t-overflow">${obj.balance}</li><li class="clear"></li></ul></li>`;
 	}
 	function pad(num, size) {
 		var s = num+"";
 		while (s.length < size) s = "0" + s;
 		return s;
+	}
+	function purge() {
+		GM_setValue("history","{}");
+		location.reload();
 	}
 })();
