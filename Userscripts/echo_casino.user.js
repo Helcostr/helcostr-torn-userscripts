5977003d103f8b07bc653fca13838b2ed5cb10a4
Echoblast53 <arc275555@gmail.com>
2018-04-15 23:35:41 -0700

    Create echo_casino.user.js
    Hello World. To be done:
    -Log all XHR casino related activities as events (wins and loss)
    -Create a dummy timestamp, replacing it with a more accurate timestamp.
    -Match custom log with official log via Results, cash, and timestamp (need to write algorithm to match).

---
+++ Userscripts/echo_casino.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Casino Log Tracker
+// @version      0.1
+// @description  Infinitly Increase Memory of Casino
+// @author       Echoblast53
+// @match        https://www.torn.com/loader.php
+// @grant        GM_getValue
+// @grant        GM_setValue
+// ==/UserScript==
