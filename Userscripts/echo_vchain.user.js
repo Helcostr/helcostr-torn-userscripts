921cf5b344f48a5994acac62cddce1b31bd71d98
Echoblast53 <arc275555@gmail.com>
2019-01-30 21:39:29 -0800

    Update echo_vchain.user.js
    Add my faction to the limiter.

--- Userscripts/echo_vchain.user.js
+++ Userscripts/echo_vchain.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Torn Vocal Chainerz
-// @version      0.2
+// @version      0.3
 // @description  Speak up when 1 minute is left on the Torn Chain
 // @author       Helcostr
 // @match        https://www.torn.com/factions.php*
@@ -8,53 +8,56 @@
 // @run-at      document-start
 // @updateURL   https://github.com/Echoblast53/echoblast53-torn-userscripts/raw/master/Userscripts/echo_vchain.user.js
 // ==/UserScript==
-
 (function() {
     'use strict';
 
-const original_fetch = fetch;
-let warning = false;
-window.fetch = async (input, init) => {
-    const response = await original_fetch(input, init);
-    if (response.url.search('faction_wars.php') != -1) {
-        if (response.url.search('step=getwardata') != -1) {
-            const clone = response.clone();
-            let payload = typeof init.body != 'undefined'?JSON.parse(init.body):{};
+    const original_fetch = fetch;
+    let warning = false;
+    window.fetch = async (input, init) => {
+        const response = await original_fetch(input, init);
+        if (response.url.search('faction_wars.php') != -1) {
+            if (response.url.search('step=getwardata') != -1) {
+                const clone = response.clone();
+                let payload = typeof init.body != 'undefined' ? JSON.parse(init.body) : {};
 
-            clone.json().then((response)=>{
-              var seconds = response.wars[0].data.chainBar.end;
-              if (seconds < 70) {
-                  warning = true;
-                  speak(secToTime(seconds));
-              } else if (warning) {
-                  warning = false;
-                  speak('Hit Confirmed');
-              }
+                clone.json().then((response) => {
+                    if (response.wars[0].data.chain.factionID == 8500) {
+                        var seconds = response.wars[0].data.chainBar.end;
+                        if (seconds < 70) {
+                            warning = true;
+                            speak(secToTime(seconds));
+                        } else if (warning) {
+                            warning = false;
+                            speak('Hit Confirmed');
+                        }
 
-            });
+                    }
+                });
+            }
         }
+        return response;
     }
-    return response;
-}
-function speak(text) {
-    var msg = new SpeechSynthesisUtterance();
-    var voices = window.speechSynthesis.getVoices();
-    msg.voice = voices[0]; // Note: some voices don't support altering params
-    msg.voiceURI = 'native';
-    msg.volume = 1; // 0 to 1
-    msg.rate = 1.5; // 0.1 to 10
-    msg.pitch = 1; //0 to 2
 
-    msg.lang = 'en-US';
-    msg.text = text;
+    function speak(text) {
+        var msg = new SpeechSynthesisUtterance();
+        var voices = window.speechSynthesis.getVoices();
+        msg.voice = voices[0]; // Note: some voices don't support altering params
+        msg.voiceURI = 'native';
+        msg.volume = 1; // 0 to 1
+        msg.rate = 1.5; // 0.1 to 10
+        msg.pitch = 1; //0 to 2
 
-    speechSynthesis.speak(msg);
-}
-function secToTime(duration) {
-  var seconds = parseInt((duration ) % 60),
-    minutes = parseInt((duration / (60)) % 60),
-    hours = parseInt((duration / (60 * 60)) % 24);
+        msg.lang = 'en-US';
+        msg.text = text;
 
-  return (hours!=0?hours + ' hours ':'') + (minutes!=0?minutes + ' minutes ':'') + (seconds!=0?seconds:'');
-}
+        speechSynthesis.speak(msg);
+    }
+
+    function secToTime(duration) {
+        var seconds = parseInt((duration) % 60),
+            minutes = parseInt((duration / (60)) % 60),
+            hours = parseInt((duration / (60 * 60)) % 24);
+
+        return (hours != 0 ? hours + ' hours ' : '') + (minutes != 0 ? minutes + ' minutes ' : '') + (seconds != 0 ? seconds : '');
+    }
 })();
