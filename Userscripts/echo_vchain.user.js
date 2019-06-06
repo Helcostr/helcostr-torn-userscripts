9d0847a2f655983dd062c20f13ef354eee231633
Echoblast53 <arc275555@gmail.com>
2019-06-06 00:15:46 -0700

    Update echo_vchain.user.js
    Prevent overlap (and add a pending prefix notification for hits that are definitely going to finish before the chain expires, if the active user doesn't get put into hospital in a guardian move)

--- Userscripts/echo_vchain.user.js
+++ Userscripts/echo_vchain.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Torn Vocal Chainerz
-// @version      0.4
+// @version      0.5
 // @description  Speak up when 1 minute is left on the Torn Chain
 // @author       Helcostr
 // @match        https://www.torn.com/*
@@ -13,8 +13,8 @@
 
     setInterval(async ()=>{
         let time = localStorage.getItem("vc_faction_keepalive");
-        if (time==null || parseInt(time)+10000<(new Date).getTime())
-            speak("Warning");
+        if (time == null || parseInt(time)+10000<(new Date).getTime())
+            speak("Warning","vc_faction_warning");
     },2500);
     const original_fetch = fetch;
     let warning = false;
@@ -29,12 +29,15 @@
                     if (response.wars[0].data.chain.factionID == 8500) {
                         localStorage.setItem("vc_faction_keepalive",(new Date).getTime());
                         var seconds = response.wars[0].data.chainBar.end;
+                        var recents = response.warDesc.recentAttacks;
                         if (seconds < 70) {
+                            if (recents.some(e=> e.result == "None" && e.finishedTimestamp+30000 < response.wars[0].data.chain.end))
+                                speak("Pending","vc_faction_assist");
                             warning = true;
-                            speak(secToTime(seconds));
+                            speak(secToTime(seconds),"vc_faction_main");
                         } else if (warning) {
                             warning = false;
-                            speak('Hit Confirmed');
+                            speak('Hit Confirmed',"vc_faction_main");
                         }
 
                     }
@@ -44,19 +47,23 @@
         return response;
     }
 
-    function speak(text) {
-        var msg = new SpeechSynthesisUtterance();
-        var voices = window.speechSynthesis.getVoices();
-        msg.voice = voices[0]; // Note: some voices don't support altering params
-        msg.voiceURI = 'native';
-        msg.volume = 1; // 0 to 1
-        msg.rate = 1.5; // 0.1 to 10
-        msg.pitch = 1; //0 to 2
+    function speak(text,overlap_key) {
+        let time = localStorage.getItem(overlap_key);
+        if (time == null || parseInt(time)+2500<(new Date).getTime()) {
+            var msg = new SpeechSynthesisUtterance();
+            var voices = window.speechSynthesis.getVoices();
+            msg.voice = voices[0]; // Note: some voices don't support altering params
+            msg.voiceURI = 'native';
+            msg.volume = 1; // 0 to 1
+            msg.rate = 1.5; // 0.1 to 10
+            msg.pitch = 1; //0 to 2
 
-        msg.lang = 'en-US';
-        msg.text = text;
+            msg.lang = 'en-US';
+            msg.text = text;
 
-        speechSynthesis.speak(msg);
+            speechSynthesis.speak(msg);
+            localStorage.setItem(overlap_key,(new Date).getTime());
+        }
     }
 
     function secToTime(duration) {
