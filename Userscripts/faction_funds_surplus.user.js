6e8b93f2af37f4eda659f81249cef05f767c21fc
Helcostr <helcostr@gmail.com>
2020-02-22 00:48:59 -0800

    Update faction_funds_surplus.user.js
    *Fix double negative bug.
    *Target users' money (after a hyperlink), instead of randomly targeting, (to avoid clashing with DKK's script)
    *Streamline code

--- Userscripts/faction_funds_surplus.user.js
+++ Userscripts/faction_funds_surplus.user.js
@@ -1,52 +1,49 @@
 // ==UserScript==
 // @name         Faction Funds Surplus
-// @version      0.0.3
+// @version      1.0.0
 // @description  Display's faction funds surplus written by nym (branch off of 0.3 https://greasyfork.org/en/scripts/376805-faction-funds-surplus)
-// @author       Helcostr [1934501]
+// @author       Helcostr
 // @match        https://www.torn.com/factions.php*
 // @grant        none
 // @run-at       document-idle
-// @updateURL    https://github.com/Helcostr/helcostr-torn-userscripts/raw/master/Userscripts/faction_funds_surplus.user.js
+// @updateURL    https://github.com/Echoblast53/echoblast53-torn-userscripts/raw/master/Userscripts/faction_funds_surplus.user.js
 // ==/UserScript==
 
 (() => {
-	var $ = window.jQuery;
+	let $ = window.jQuery;
 
+	//Trigger on ajax complete
 	$(document).ajaxComplete((event,xhr,settings)=>{
-		if (settings.url.search("factions.php") != -1) {
+		if (settings.url.search("factions.php") != -1)
 			appendSurplus();
-		}
 	});
 
-	function appendSurplus() {
-		if ($('#surplusInfo').size() == 0) $('.money-depositors').append('<div class="info" id="surplusInfo"><li>Deposited: <span id="deposited"></span></li><li>Surplus: <span id="surplusValue"</span> </li></div>');
-		var total_money = parseInt($('span[data-faction-money]').attr('data-faction-money'));
-		var total_deposited = 0;
-		var total_borrowed = 0;
-		var list_deposited = $(".money-depositors .money").map(function(){return parseInt($(this).attr("data-value"));}).get();
-		console.log(list_deposited);
-		list_deposited.forEach((val) => {
-			if (val < 0) total_borrowed += val;
-			else total_deposited += val;
+	const appendSurplus = ()=>{
+		//Inject HTML Once
+		if ($('#surplusInfo').size() == 0) $('.money-depositors').append('<li class="depositor" id="surplusInfo"><div class="clearfix"><div class="user name" style="width: 147px;">Deposit: <span class="money" id="deposited"></span></div><div class="amount"><div class="show">Surplus: <span class="money" id="surplusValue"></span></div></div></div></li>');
+		
+		//Set Values (And Accumulators)
+		let total_money = parseInt($('span[data-faction-money]').attr('data-faction-money'));
+		let total_positive = 0;
+		let total_negative = 0;
+
+		//Add to accumulators
+		$(".money-depositors a+.amount .money").get().forEach((e)=>{
+			let val = parseInt($(e).attr("data-value");
+			if (val < 0)
+				total_borrowed += val;
+			else
+				total_deposited += val;
 		});
 
-		console.log('Total money = ' + total_money);
-		console.log('Total deposited = ' + total_deposited);
-		console.log('Total borrowed = ' + total_borrowed);
+		//Set output value
 		$("#deposited").text('$' + formatMoney(total_deposited));
 		$("#surplusValue").text('$'+ formatMoney(total_money - total_deposited));
-
 	}
 
-	function formatMoney(amount, thousands = ",") {
-
-			const negativeSign = amount < 0 ? "-" : "";
-
-			let i = amount.toString();
-			let j = (i.length > 3) ? i.length % 3 : 0;
-
-			return negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands);
+	const formatMoney = (amt, delim = ",") => {
+		const negativeSign = amt < 0 ? "-" : "";
+		let val = Math.abs(amt).toString();
+		return negativeSign + val.replace(/\d{1,3}(?=(\d{3})+(?=\.))/g,"$&"+delim);//
 	};
-
-
 })();
