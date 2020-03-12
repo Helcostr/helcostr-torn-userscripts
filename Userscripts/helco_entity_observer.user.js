4110e180ba8fe60fa2cd2b92e2d4b949ddb43f52
Helcostr <helcostr@gmail.com>
2020-03-12 00:13:14 -0700

    Update helco_entity_observer.user.js

--- Userscripts/helco_entity_observer.user.js
+++ Userscripts/helco_entity_observer.user.js
@@ -1,25 +1,42 @@
 // ==UserScript==
 // @name         Historical API Loader
-// @version      0.1
+// @version      0.2
 // @description  Provide IDs of any active entity (faction/player) to Helcostr's tracker
+// @updateURL    https://github.com/Helcostr/helcostr-torn-userscripts/raw/master/Userscripts/helco_entity_observer.user.js
 // @author       Helcostr [1934501]
 // @match        https://www.torn.com/*
 // @run-at       document-end
 // @grant        GM_xmlhttpRequest
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
 // @connect      script.google.com
 // @connect      script.googleusercontent.com
 // ==/UserScript==
 
+GM_registerMenuCommand("Set API Key", apiKey, null);
+function apiKey() {
+    let key = prompt("Please enter your API key",GM_getValue("Key"));
+    if (key.length != 16)
+        alert("Bad Key?");
+    else
+        GM_setValue("Key",key);
+}
 setTimeout(()=> {
     'use strict';
     const sendTo = load=>{
-        console.log(load);
+        let key = GM_getValue("Key","");
+        if (key.length != 16) {
+            apiKey();
+            return;
+        }
         return new Promise((res,rej)=>{
             const url = "https://script.google.com/macros/s/AKfycbyiBCWAdzkzTG5oZxOzE5ZFZCQPIb-uqvH0UYI32l2fqoDtjPI/exec";
             GM_xmlhttpRequest({
                 method:"POST",
                 data:JSON.stringify({
                     pwd:"@6m)E7ukFCQ*RTT8",
+                    api:key,
                     data:load
                 }),
                 onload:(r)=>{
