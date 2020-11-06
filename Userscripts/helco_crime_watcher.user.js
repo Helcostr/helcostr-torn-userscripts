555caa24bbb1baf994a2b1ca2410d6e1f9b7537e
Helcostr <helcostr@gmail.com>
2020-11-06 11:56:40 -0800

    add reason

--- Userscripts/helco_crime_watcher.user.js
+++ Userscripts/helco_crime_watcher.user.js
@@ -85,7 +85,7 @@
 				if (x.status === 200) {
 					if (storeKey) localStorage.removeItem(storeKey);
 				} else if (x.status === 400 || x.status === 500)
-					alert("😱😱😱 Contact Helcostr with this message: "+x.status+" on Crime Watcher "+JSON.parse(x.responseText).+" 😱😱😱");
+					alert("😱😱😱 Contact Helcostr with this message: "+x.status+" on Crime Watcher "+JSON.parse(x.responseText).reason+" 😱😱😱");
 			}
 		});
 	};
