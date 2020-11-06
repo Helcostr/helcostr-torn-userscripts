66b7b25668b97b6ac1280ed96a21c449cece29ed
Helcostr <helcostr@gmail.com>
2020-11-06 03:40:49 -0800

    helco_crime_watcher add catchers
    add incorrect id relationship
    add bad API key catcher (loud on player data fetch)
    add perks,bars error catcher (silent)

--- Userscripts/helco_crime_watcher.user.js
+++ Userscripts/helco_crime_watcher.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Crime Watcher
-// @version      2.0
+// @version      2.1
 // @description  Watch crimes done in Torn (written for Tampermonkey)
 // @author       Helcostr [1934501]
 // @match        https://www.torn.com/crimes.php*
@@ -15,15 +15,18 @@
 
 /*eslint no-unused-vars: 0*/
 (function() {
-    'use strict';
+	'use strict';
+	let $ = unsafewindow.$ || $;
+
 	//My Gets and Sets
 	const getKey = ()=>GM_getValue("key","");
 	const setKey = key=>{
 		GM_setValue("key",key);
 		GM_setValue("playerData",null);
 		GM_setValue("cacheTS",0);
+		GM_setValue("cache",null);
 	};
-	const getCache = ()=>GM_getValue("cache","{}");
+	const getCache = ()=>GM_getValue("cache",null);
 	const getCacheTS = ()=>GM_getValue("cacheTS",0);
 	const getPlayerData = ()=>GM_getValue("playerData",null);
 	const setPlayerData = data=>GM_setValue("playerData",data);
@@ -33,31 +36,39 @@
 	};
 	//Api Fetching
 	const api = selections=>{
-        return new Promise((res,rej)=>{
-            const url = "https://api.torn.com/user/?selections="+selections+"&key=";
-            GM_xmlhttpRequest({
-                method:"GET",
-                url:url+getKey(),
+		return new Promise((res,rej)=>{
+			const url = "https://api.torn.com/user/?selections="+selections+"&key=";
+			GM_xmlhttpRequest({
+				method:"GET",
+				url:url+getKey(),
 				onload:(r)=>{
-                    try {
-                        let data = JSON.parse(r.responseText);
-                        res(data);
-                    } catch (e) {
-                        rej(e.toString() + "\n" + r.responseText);
-                    }
+					try {
+						let data = JSON.parse(r.responseText);
+						res(data);
+					} catch (e) {
+						rej(e.toString() + "\n" + r.responseText);
+					}
 				}
-            });
-        });
+			});
+		});
 	};
 	//Sending to server stuff
-    const sendTo = (load,storeKey)=>{
+	const sendTo = (load,storeKey)=>{
 		const url = "https://elimination.me/api/jts/crime";
 		let basic = getPlayerData();
 		let perks = getCache();
-		if (load.player !== basic.player_id);
 		const takeCare = ()=>{
 			if (!storeKey)localStorage.setItem("crime_watch:"+Date.now(),JSON.stringify(load));
 		};
+		if (perks === null)
+			takeCare();
+		if (load.player !== basic.player_id) {
+			takeCare();
+			setKey("");
+			if(window.confirm("Wrong API Key. Fetch now?"))
+				window.location.href="https://www.torn.com/preferences.php#tab=api"; //Use player input to go to the prefernce page
+			return;
+		}
 		GM_xmlhttpRequest({
 			method:"POST",
 			data:JSON.stringify({
@@ -75,13 +86,14 @@
 					console.warn("😱😱😱 Contact Helcostr with this message: "+x.status+" on Crime Watcher 😱😱😱");
 			}
 		});
-    };
+	};
 	//Emergency Recovery
 	Object.keys(localStorage).filter(e=>e.startsWith("crime_watch:")).forEach(e=>{
 		sendTo(JSON.parse(localStorage.getItem(e)),e);
 	});
 	//Ask user to do Preference stuff!
-	if (getKey() === "" && window.location.pathname === "/crimes.php" && window.confirm("Load API Key Into Crime Watcher?")) window.location.href="https://www.torn.com/preferences.php#tab=api"; //Use player input to go to the prefernce page
+	if (getKey() === "" && window.location.pathname === "/crimes.php" && window.confirm("Load API Key Into Crime Watcher?"))
+		window.location.href="https://www.torn.com/preferences.php#tab=api"; //Use player input to go to the prefernce page
 	//Preference stuff!
 	if (window.location.pathname === "/preferences.php") {
 		let quick = true; //Prep API Intercept
@@ -106,45 +118,51 @@
 
 	//API Preload
 	if (getPlayerData() === null)
-		api("basic").then(setPlayerData);
+		api("basic").then(d=>{
+			if ("error" in d && confirm(d.error.error+" Get API Key again?"))
+				window.location.href="https://www.torn.com/preferences.php#tab=api"; //Use player input to go to the prefernce page
+			else
+				setPlayerData(d);
+		});
 	if (getCacheTS()+1000*60*60*24<Date.now())
 		api("perks,bars").then(data=>{
+			if ("error" in data)
+				return;
 			let newData = {};
 			Object.keys(data).forEach(key=>{
 				let type = data[key].length;
 				if (type) newData[key] = data[key].filter(e=>/(nerve|crime|success)/i.test(e));
 			});
-			console.log(newData);
 			setCache({
 				...newData,
 				maximum_nerve:data.nerve.maximum
 			});
 		});
 	//Cookie stuff
-    const getCookie = (name)=>{
-        var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-        if (match) return match[2];
-    };
+	const getCookie = (name)=>{
+		var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+		if (match) return match[2];
+	};
 	//Intercepter for Crimes
-    $(document).ajaxComplete((event, xhr, settings)=>{
-        if (settings.url.search("docrime") == -1
-            || typeof settings.data == "undefined"
-            || settings.data.search("nervetake") == -1)
-            return; //Skip
+	$(document).ajaxComplete((event, xhr, settings)=>{
+		if (settings.url.search("docrime") == -1
+			|| typeof settings.data == "undefined"
+			|| settings.data.search("nervetake") == -1)
+			return; //Skip
 		//Grab payload and package it
-        let dataArray = settings.data.split("&");
-        let dataObj = {};
-        dataArray.forEach(e=>{
-            let kv = e.split("=");
-            dataObj[kv[0]] = kv[1];
-        });
-        let msg = $(xhr.responseText).find('[class$="-message"]');
-        let payloadOut = {
-            player: parseInt(getCookie("uid")),
-            ...dataObj,
-            result: msg.text(),
-            status: msg.attr("class").slice(0,msg.attr("class").search("-message"))
-        };
+		let dataArray = settings.data.split("&");
+		let dataObj = {};
+		dataArray.forEach(e=>{
+			let kv = e.split("=");
+			dataObj[kv[0]] = kv[1];
+		});
+		let msg = $(xhr.responseText).find('[class$="-message"]');
+		let payloadOut = {
+			player: parseInt(getCookie("uid")),
+			...dataObj,
+			result: msg.text(),
+			status: msg.attr("class").slice(0,msg.attr("class").search("-message"))
+		};
 		sendTo(payloadOut);
-    });
+	});
 })();
