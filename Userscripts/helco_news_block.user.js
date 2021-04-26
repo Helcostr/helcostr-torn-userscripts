123e54b676618f0aa1fcdc4896b43ab0facc7c82
Helcostr <helcostr@gmail.com>
2021-04-26 07:06:38 -0700

    Update helco_news_block.user.js
    I'm just having fun

--- Userscripts/helco_news_block.user.js
+++ Userscripts/helco_news_block.user.js
@@ -12,6 +12,7 @@
 
 (function() {
     'use strict';
+    String.prototype.splice = function(idx, rem, str) {return this.slice(0, idx) + str + this.slice(idx + Math.abs(rem));};
     const random = length =>{
         let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
         let str = '';
@@ -19,19 +20,23 @@
             str += chars.charAt(Math.floor(Math.random() * chars.length));
         return str;
     };
+    const string = "This is a frozen message. Disable the userscript to view the results.";
+    const dance = (string,i)=>{
+        return string.splice(i,0,['\ud83d\udc83','\ud83d\udd7a\ud83c\udffd'][i%2]);
+    };
     const original_fetch = unsafeWindow.fetch;
     unsafeWindow.fetch = async (input, init) => {
-        const payload = () => ({
+        const payload = (i) => ({
             "ID":random(20),
-            "date":"24\/04\/21",
+            "date":"24/04/21",
             "time":"19:31:06",
-            "message":"This is a frozen message. Disable the userscript to view the results."
+            "message":dance(string,i)
         });
         const genList = [];
-        for (let i = 0; i < 30; i++)
-            genList.push(payload());
-        if (/page\.php\?sid=factionsNews&step=list/.test(input));
-            return new Response(JSON.stringify({"success":true,"list":genList,"startFrom":"1614973672631832166"}))
+        for (let i = 0; i < string.length+1; i++)
+            genList.push(payload(i));
+        if (/page\.php\?sid=factionsNews&step=list/.test(input))
+            return new Promise(res=>setTimeout(res,500)).then(()=>new Response(JSON.stringify({"success":true,"list":genList,"startFrom":"1614973672631832166"})));
         return original_fetch(input, init);
     };
 })();
